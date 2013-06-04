39e8458e298eeb77246ad4b2a573b2922a8d12c6
markets <srmarc.ai@gmail.com>
2013-06-04 20:32:10 +0200

    remove Gemfile.lock from repo

--- lib/invisible_captcha/view_helpers.rb
+++ lib/invisible_captcha/view_helpers.rb
@@ -1,11 +1,11 @@
 module InvisibleCaptcha
   module ViewHelpers
 
-    def invisible_captcha model_name, attr_name
+    def invisible_captcha(model_name, attr_name)
       html_ids = []
       { :"#{model_name}" => 'If you are a human, do not fill in this field' }.collect do |field, label|
         html_ids << (html_id = "#{field}_#{Time.now.to_i}")
-        content_tag :div, :id => html_id do
+        content_tag(:div, :id => html_id) do
           content_tag(:style, :type => 'text/css', :media => 'screen', :scoped => "scoped") do
             "#{html_ids.map { |i| "##{i}" }.join(', ')} { display:none; }"
           end +

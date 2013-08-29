55b379a16a669371b637f6cfa42244bf2d8d16d9
markets <srmarc.ai@gmail.com>
2013-08-30 01:08:52 +0200

    more readable code

--- lib/invisible_captcha/view_helpers.rb
+++ lib/invisible_captcha/view_helpers.rb
@@ -5,22 +5,24 @@ module InvisibleCaptcha
       build_invisible_captcha(model_name.to_s, method.to_s)
     end
 
-    protected
+    private
 
     def build_invisible_captcha(model_name, method)
-      html_ids = []
-      { :"#{model_name}" => 'If you are a human, do not fill in this field' }.collect do |field, label|
-        html_ids << (html_id = "#{field}_#{Time.now.to_i}")
-        content_tag(:div, :id => html_id) do
-          content_tag(:style, :type => 'text/css', :media => 'screen', :scoped => "scoped") do
-            "#{html_ids.map { |i| "##{i}" }.join(', ')} { display:none; }"
-          end +
-            label_tag("#{field}_#{method}", label) +
-            text_field_tag("#{model_name}[#{method}]")
-        end
-      end.join.html_safe
+      label      = 'If you are a human, ignore this field'
+      html_id    = "#{model_name}_#{Time.now.to_i}"
+
+      content_tag(:div, :id => html_id) do
+        insert_inline_css_for(html_id) +
+        label_tag("#{model_name}_#{method}", label) +
+        text_field_tag("#{model_name}[#{method}]")
+      end.html_safe
     end
 
+    def insert_inline_css_for(container_id)
+      content_tag(:style, :type => 'text/css', :media => 'screen', :scoped => 'scoped') do
+       "##{container_id} { display:none; }"
+      end
+    end
   end
 end
 

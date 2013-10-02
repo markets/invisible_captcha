05617ef845aeaa856159c50d082f8ecee9103b1a
Marc Anguera Insa <srmarc.ai@gmail.com>
2013-10-02 12:09:13 +0200

    require files from relative path

--- lib/invisible_captcha.rb
+++ lib/invisible_captcha.rb
@@ -1,8 +1,7 @@
-GEM_PATH = File.dirname(__FILE__) + "/invisible_captcha"
-require "#{GEM_PATH}/controller_helpers.rb"
-require "#{GEM_PATH}/view_helpers.rb"
-require "#{GEM_PATH}/form_helpers.rb"
-require "#{GEM_PATH}/validator.rb"
+require 'invisible_captcha/controller_helpers.rb'
+require 'invisible_captcha/view_helpers.rb'
+require 'invisible_captcha/form_helpers.rb'
+require 'invisible_captcha/validator.rb'
 
 module InvisibleCaptcha
   # Default sentence for humans if text field is visible

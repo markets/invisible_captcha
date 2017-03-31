85832efa66da25b5ce32e53ecc151099c9cd43ff
Alberto Garcia Cabeza <alberto@decabeza.es>
2017-03-31 10:27:36 +0200

    improves visual honeypots css

--- lib/invisible_captcha/view_helpers.rb
+++ lib/invisible_captcha/view_helpers.rb
@@ -22,10 +22,12 @@ module InvisibleCaptcha
 
       honeypot = honeypot ? honeypot.to_s : InvisibleCaptcha.get_honeypot
       label    = options[:sentence_for_humans] || InvisibleCaptcha.sentence_for_humans
+      visual_honeypots = options[:visual_honeypots] || InvisibleCaptcha.visual_honeypots
+      inline_style = visual_honeypots ? "" : "display: none;"
+
       html_id  = generate_html_id(honeypot, scope)
 
-      content_tag(:div, :id => html_id) do
-        concat visibility_css(html_id, options)
+      content_tag(:div, :id => html_id, style: inline_style) do
         concat label_tag(build_label_name(honeypot, scope), label)
         concat text_field_tag(build_text_field_name(honeypot, scope))
       end
@@ -33,18 +35,6 @@ module InvisibleCaptcha
 
     def generate_html_id(honeypot, scope = nil)
       "#{scope || honeypot}_#{Time.zone.now.to_i}"
-    end
-
-    def visibility_css(container_id, options)
-      visibility = if options.key?(:visual_honeypots)
-        options[:visual_honeypots]
-      else
-        InvisibleCaptcha.visual_honeypots
-      end
-
-      content_tag(:style, :type => 'text/css', :media => 'screen', :scoped => 'scoped') do
-        "##{container_id} { display:none; }" unless visibility
-      end
     end
 
     def build_label_name(honeypot, scope = nil)

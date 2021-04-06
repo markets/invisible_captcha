74caa616f8d63db09cf0c50eecdfcba587264d03
Marc Anguera Insa <srmarc.ai@gmail.com>
2021-04-06 22:07:45 +0200

    Simplify testing app and DRY Appraisals file

--- spec/dummy/app/assets/config/manifest.js
+++
@@ -1,2 +0,0 @@
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -14,7 +14,7 @@ Dummy::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -30,9 +30,6 @@ Dummy::Application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   # config.assets.digest = true
-
-  # quiet assets
-  config.assets.quiet = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.

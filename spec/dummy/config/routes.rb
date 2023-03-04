24393b7bb197c57d74a98048a42c65fcd5caffac
Marc Anguera <srmarc.ai@gmail.com>
2023-03-04 22:47:05 +0100

    [ci] simplify ci.yml workflow (#121)

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :topics do
     post :publish, on: :member
-
     post :rename, on: :collection
     post :categorize, on: :collection
     post :copy, on: :collection
